73ac3d518fe41f47b9154d4846199b53dcf6706b
faisal88899 <faisaljameel88899@gmail.com>
2024-05-07 15:55:41 +0500

    my-first-commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// alert ("i am Arsalan ajshjfnjdfhdhfgbvhdffgdjfhjksdhksfsjkhsdjkfhs")
+
+
+// let lastName = "faisal   "
+
+// console.log (lastName)
+
+
+var lastName = "arsalan     ";
+var last_Name = "faisal      ";
+var lastName2 = "Umair       ";
+var lastName3 = "Malik       ";
+var lastName4 = "shoaib     ";
+
+console.log (lastName3 + lastName2 + lastName4 + last_Name + lastName)
+
+let num  = 987655444;
+console.log(typeof(num))
+;
+l
+console.log(firstName)
+ 
+
+var name = "faisal "
+
+const num1 = [1, 2, 3, 5, 6, 88];
