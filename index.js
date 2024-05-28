e4b6fc553a0637dfe409678b28bfcad38eabf885
Faisal Jameel <faisaljameel88899@gmail.com>
2024-05-28 16:33:53 +0500

    js practice

--- index.js
+++ index.js
@@ -1,34 +1,348 @@
-// alert ("i am Arsalan ajshjfnjdfhdhfgbvhdffgdjfhjksdhksfsjkhsdjkfhs")
+// let name = +prompt("enter valid number")
+// if (name >= 18){
+//     console.log(" your are eligible ")
+// } else{
+//      console.log("you are not eligible")
+// }
 
 
-// let lastName = "faisal   "
+// let number = prompt("enter your lucky number");
+// let autonumber = Math.round(Math.random()  * 5);
 
-// console.log (lastName)
+// if ( number === autonumber){
+//     console.log("you are win");
+// } else{
+//     console.log("you are loss !" + autonumber);
+// }
+
+// let num = null;
+// let num2 = "" ;
+
+// console.log(num + num2)
+
+// let num = 34;
+// let num2 = "34";
+// console.log(num === num2)
+
+// let firstName = 12;
+// let lastName = 13;
+
+// console.log(firstName != lastName)
+
+// let first = 40;
+// let last = 30;
+
+// console.log( first <= last)
+
+// let percentage = 99;
+
+// if (percentage  >= 80 && percentage < 100 ){
+// document.write("your are marit");
+// }else if (percentage  >= 60 && percentage < 79 ){
+//     document.write("your are first division");
+//     } else if (percentage  >= 34 && percentage < 59 ){
+//         document.write("your are marit");
+//         }else if (percentage  < 33 ){
+//             document.write("your are marit");
+
+//             } else{
+//                 document.write("please enter valid number");
+
+//             } 
+
+// let marks = prompt("please enter a number");
+
+// if (marks >= 80 && marks < 100){
+//     console.log("you are marit");
+// } else if (marks >= 60 && marks < 79){
+//     console.log("you are 1st division");
+// } else if (marks >= 45 && marks < 59){
+//     console.log("you are 2nd division");
+// } else if (marks >= 33 && marks < 44){
+//     console.log("you are 3rd diviosion");
+// } else{
+//     console.log("please enter valid number");
+// }
 
 
-var lastName = "arsalan     ";
-var last_Name = "faisal      ";
-var lastName2 = "Umair       ";
-var lastName3 = "Malik       ";
-var lastName4 = "shoaib     ";
-var lastName4 = "shoaib     ";
-var lastName4 = "shoaib     ";
-var lastName4 = "shoaib     ";
-var lastName4 = "shoaib     ";
+// let name = 66;
+// let name2 = 67;
 
-console.log (lastName3 + lastName2 + lastName4 + last_Name + lastName)
+// // console.log( name == name2)
+// if (name != name2){
+//     document.write("you are eligible ");
+// } else{
+//     document.write("you are not eligible");
+// // }
 
-let num  = 987655444;
-console.log(typeof(num))
-;
-l
-console.log(firstName)
- 
+// let name = +prompt(" please enter your number")
+// let name2 = Math.round(Math.random() * 10)
+// console.log( name + name2)
 
-var name = "faisal "
+// let table = 8;
 
-const num1 = [1, 2, 3, 5, 6, 88];
-var faisal = "aligohar"
+// for (let i = 1; i <= 10; i++){
+//     console.log(table + " X " + i + " = " + table *i)
+// }
+// let counting = 2;
 
-let title ="product NamedNodeMap"
-console.log(title)
+// for (let i = 0; i <= 10; i++){
+//     console.log(counting + "+" + i + "=" +  counting + i )
+// }
+
+
+
+// // let faisal = +prompt("enter your number");
+// // switch (faisal) {
+// //     case 100:
+// //         console.log("faisal is available")
+// //         break;
+// //         case 1000:
+// //         console.log("faisal is not available")
+// //         break;
+// //         case 10000:
+// //         console.log("faisal is also available")
+// //         break;
+
+// //     default:
+// //         console.log("aisal")
+// //         break;
+// }
+
+// let day =1;
+// switch (day) {
+    
+//     case 0:
+//         console.log("today is monday")
+//         break;
+//         case 1:
+//         console.log("today is tuesday")
+//         break;
+//         case 2:
+//         console.log("today is wednessday")
+//         break;
+//         case 3:
+//         console.log("today is thursday")
+//         break;
+//         case 4:
+//         console.log("today is friday")
+//         break;
+//         case 5:
+//         console.log("today is saturday")
+//         break;
+//         case 6:
+//         console.log("today issunday")
+//         break;
+
+//     default:
+//         console.log("please enter valid number")
+//         break;
+// }
+
+// /
+// let data = emloyee(7);
+//
+// // let assignment = 500;
+// function first(){
+//     console.log("hello everybody");
+// }
+
+// function last(){
+//     console.log("how are you")
+// }
+// first()
+// last()
+
+// let table = 5;
+// for (let a = 0; a <=10; a++){
+//     console.log(table+ " X " + a + "=" + table *a)
+// }
+
+// let per = prompt("enter your number")
+
+// if (per >= 80 && per <= 100){
+//     console.log("you are 1st position")
+
+// }else(console.log("please enter valid number"))
+
+// let name = +prompt("please enter your number")
+// let name2 = Math.round(Math.random() *5)
+
+// console.log(name +  name2)
+
+// function ali (first,name){
+//     console.log("hello  " + first + name)
+// }
+// ali("faisal ","arain ")
+
+// function alibaba(f1,f2,f3,f4){
+//     console.log("faisal " + f1 + f2 + f3 + f4)
+// }
+// alibaba("bhai", " kia hal he" , "koi", "he")
+
+// function add (a1,a2,a3,a4){
+//     console.log(a1+a2+a3+a4);
+// }
+// add(24,25,25,66)
+// add(30,50,15,67)
+// add(55,600,500,567)
+
+
+// function name2 (math,eng,urdu){
+//     let s = math + eng + urdu;
+
+
+//     return s;
+// }
+// function percentage(tt){
+//     let per = tt/300 *100;
+    
+//     console.log(per)
+// }
+
+
+// let total = (40+40+40);
+//  percentage(total);
+
+// function abid (math,eng,urdu){
+//     var ali = math+eng+urdu;
+//     return (ali)
+// }
+// var gohar = abid(40,34,35);
+// console.log(gohar)
+
+// function firstName(first,second,third){
+//     let last = first+second+third;
+
+//     return (last)
+// }
+// let lastName = firstName(40,40,40);
+// console.log(lastName)
+
+
+// function faisal(monday,tuesday,wednesday){
+//     console.log(monday+tuesday+wednesday);
+// }
+// faisal(40,50,50);
+// faisal(50,50,50);
+
+
+// function abidali(name1,name2){
+//     console.log(name1+name2)
+// }
+// abidali(50,50)
+// abidali(50,50)
+
+// let moth = prompt("please enter your number")
+// switch (moth) {
+//     case  0:
+//         console.log("this month is january")
+        
+//         break;
+//         case 1:
+//             console.log("this month is january")
+            
+//             break;
+
+//     default:
+//         console.log("please enter a valid number")
+//         break;
+// }
+
+// let math = prompt("please enter a number")
+// if (math >= 80 && math <= 100 ){
+//     console.log("you are eligible");
+
+// }else {
+//     console.log("you are not eligible");
+// }
+
+// function tokin(tokin1,tokin2,tokin3){
+//     let ped = tokin1+tokin2+tokin3;
+
+//     return(ped)
+// }
+
+
+
+
+
+
+
+// let register = tokin(50,100,100);
+// console.log(register)
+// register = tokin(555,555,555)
+// console.log(register)
+    
+
+// function gohar1(){
+// console.log("ali gohar meerani")
+// }
+// gohar1()
+
+// function abida(fname,lname,lastname){
+//     console.log(fname+lastname+lname)
+// }
+// abida(30,30,30)
+
+
+// function helloworld(a1,a2,a3,a4){
+//     let hello =a1+a2+a3+a4
+//     return hello
+// }
+// let wajeeha =helloworld(55,55,55,55)
+// console.log(wajeeha)
+
+
+// let name = []
+// for (let i =1; i < 10; i++){
+//     name.push("hello")
+// }
+// console.log(name)
+
+// let name = ["fasial","arslan","ameen","umair","ali","ahmed"];
+// for (let elem in name){
+//     console.log(elem);
+// // }
+
+
+// let name = ["fasial","arslan","ameen","umair","ali","ahmed"];
+
+// name.forEach(function(val, index ){
+//     console.log(val , index)
+    
+// });
+
+
+// let filteraray = [1,2,3,4,5,6,7,22];
+// let aray =filteraray.filter(function(val){
+//     if(val% 2 != 0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }) 
+// console.log(filteraray)
+// console.log(aray);
+
+
+let ameen =[11,22,33,34,665,687,45,576678,89,67,33,5,67,78,97,77,88,9906,89,]
+console.log(ameen);
+
+// let fasial =ameen.sort(function(a,b){
+//     if(a > b){
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+// console.log(fasial);
+
+let arslan =ameen.reduce(function( preval , currval){
+    return preval + currval ;
+
+})
+console.log(arslan);
+
+
+
